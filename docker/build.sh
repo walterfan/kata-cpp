a9d96be6996969a36d98a165a39563f6b3400caf
Walter Fan <walterfan@qq.com>
2022-05-22 15:45:33 +0800

    update docker file

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,4 @@
-docker image build --tag ubuntu-bcpp:auto .
-docker container run --rm ubuntu-bcpp:auto
+echo "docker image build --tag ubuntu-bcpp:auto ."
+echo "docker container run --rm ubuntu-bcpp:auto"
+
+echo "docker run -it -v $PWD/../kata:/opt/bcpp/kata --name ubuntu-bcpp ubuntu-bcpp"
